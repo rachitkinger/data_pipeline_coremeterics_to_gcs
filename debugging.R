6445116f4af821bb53877f4a8ebc825f24402d21
Rachit Kinger <rachitkinger@gmail.com>
2019-03-24 07:48:10 +0000

    using this as scratch-pad and debugging script

---
+++ debugging.R
@@ -0,0 +1,16 @@
+dapi <- report_details$original_api[1]
+wapi <- report_details$original_api[6]
+mapi <- report_details$original_api[7]
+
+x1 <- report_details$start_period[1]
+y1 <- report_details$start_period[1]
+f1 <- report_details$frequency[1]
+x2 <- report_details$start_period[6]
+y2 <- report_details$start_period[6]
+f2 <- report_details$frequency[6]
+x3 <- report_details$start_period[7]
+y3 <- report_details$start_period[7]
+f3 <- report_details$frequency[7]
+
+d <- get_period(x1, y1, f1)
+
