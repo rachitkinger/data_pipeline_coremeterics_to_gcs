c3b3e04372779def443df89a46d6012a49136b1f
Rachit Kinger <rachitkinger@gmail.com>
2019-03-24 07:27:58 +0000

    added return statement to get_period()

--- script.R
+++ script.R
@@ -58,8 +58,7 @@ get_period <- function(start_period, end_period, frequency) {
     frequency == "monthly" ~ get_monthly_dates(start_period, end_period)
   )
   
-  
-  
+  return(dates)
 }
 
 get_modified_api <- function(period, original_api) {
